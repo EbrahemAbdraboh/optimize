2728085a8e601500f87c08bb34a8def6ce2c3a2d
EbrahemAbdraboh <ebrahem.abdraboh.com>
2020-01-10 17:14:51 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './components/home/Home';
 import { About } from './components/about/About';
 import { Capabilities } from './components/capabilities/Capabilities';
